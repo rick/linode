bd12499606d7531fba25b8d95c1958511f5e8c5f
Rick Bradley <rick@rickbradley.com>
2009-08-05 02:50:25 -0500

    tiny tweak to use has_namespace instead of an old literal method declaration

--- lib/linode/domain.rb
+++ lib/linode/domain.rb
@@ -1,7 +1,4 @@
 class Linode::Domain < Linode
+  has_namespace :resource
   has_method :update, :create, :list, :delete
-  
-  def resource
-    @test ||= Linode::Domain::Resource.new(:api_key => api_key, :api_url => api_url)
-  end
 end
