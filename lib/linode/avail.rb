36fc258e8354798a18230d826964392f2e914fc8
Rick Bradley <rick@rickbradley.com>
2009-08-05 00:20:42 -0500

    Avail API specs and methods

--- lib/linode/avail.rb
+++ lib/linode/avail.rb
@@ -1,2 +1,17 @@
 class Linode::Avail < Linode
+  def datacenters(data ={})
+    send_request('avail.datacenters', data)
+  end
+  
+  def kernels(data ={})
+    send_request('avail.kernels', data)
+  end
+  
+  def linodeplans(data ={})
+    send_request('avail.linodeplans', data)
+  end
+  
+  def distributions(data ={})
+    send_request('avail.distributions', data)
+  end
 end
