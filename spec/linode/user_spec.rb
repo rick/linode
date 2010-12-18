f16220c59294445ee1d63117c14714e0c2a2e596
Aaron Hamid <aaron.hamid@gmail.com>
2010-12-18 13:19:12 -0500

    remove changes to tests that are no longer relevant

--- spec/linode/user_spec.rb
+++ spec/linode/user_spec.rb
@@ -15,30 +15,6 @@ describe Linode::User do
     @linode.should respond_to(:getapikey)
   end
   
-    describe "when accessing the #{action} API" do
-      it 'should allow a data hash' do
-        lambda { @linode.send(action.to_sym, {}) }.should_not raise_error(ArgumentError)
-      end
-    
-      it 'should not require arguments' do
-        lambda { @linode.send(action.to_sym) }.should_not raise_error(ArgumentError)      
-      end
-    
-      it "should request the user.#{action} action" do
-        @linode.expects(:send_unauthenticated_request).with {|api_action, data| api_action == "user.#{action}" }
-        @linode.send(action.to_sym)
-      end
-    
-      it 'should provide the data hash when making its request' do
-        @linode.expects(:send_unauthenticated_request).with {|api_action, data| data = { :foo => :bar } }
-        @linode.send(action.to_sym, {:foo => :bar})
-      end
-    
-      it 'should return the result of the request' do
-        @linode.expects(:send_unauthenticated_request).returns(:bar => :baz)      
-        @linode.send(action.to_sym).should == { :bar => :baz }      
-      end
-    end
   describe 'when returning the API key for the connection' do
     it 'should work without arguments' do
       lambda { @linode.getapikey }.should_not raise_error(ArgumentError)
