45d5ffc58e0fa27d59daae99c3111d2ba036bf88
Rick Bradley <rick@rickbradley.com>
2011-08-20 17:12:17 -0500

    and make the ostruct patch more readable

--- lib/ostruct_tweak.rb
+++ lib/ostruct_tweak.rb
@@ -1,4 +1,8 @@
 # some of our Linode API data results include a 'type' data member.
 # OpenStruct will have problems providing a .type method, so we special-case this.
 
-OpenStruct.__send__(:define_method, :type) { @table[:type] }
+class OpenStruct
+  def type
+    @table[:type]
+  end
+end
