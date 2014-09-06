aa822143c6a073a4966a8b8fda7e8064213ead5b
Rick Bradley <bradley@github.com>
2014-09-06 01:23:46 -0500

    whitespace :lipstick:

--- spec/linode/account_spec.rb
+++ spec/linode/account_spec.rb
@@ -6,40 +6,40 @@ describe Linode::Account do
     @api_key = 'foo'
     @linode = Linode::Account.new(:api_key => @api_key)
   end
-  
+
   it 'should be a Linode instance' do
     @linode.class.should < Linode
   end
-  
+
   %w(estimateinvoice list).each do |action|
     it "should allow accessing the #{action} API" do
       @linode.should respond_to(action.to_sym)
     end
-  
+
     describe "when accessing the #{action} API" do
       it 'should allow a data hash' do
         lambda { @linode.send(action.to_sym, {}) }.should_not raise_error(ArgumentError)
       end
-    
+
       it 'should not require arguments' do
         lambda { @linode.send(action.to_sym) }.should_not raise_error(ArgumentError)      
       end
-    
+
       it "should request the account.#{action} action" do
         @linode.expects(:send_request).with {|api_action, data| api_action == "account.#{action}" }
         @linode.send(action.to_sym)
       end
-    
+
       it 'should provide the data hash when making its request' do
         @linode.expects(:send_request).with {|api_action, data| data = { :foo => :bar } }
         @linode.send(action.to_sym, {:foo => :bar})
       end
-    
+
       it 'should return the result of the request' do
-        @linode.expects(:send_request).returns(:bar => :baz)      
-        @linode.send(action.to_sym).should == { :bar => :baz }      
+        @linode.expects(:send_request).returns(:bar => :baz)
+        @linode.send(action.to_sym).should == { :bar => :baz }
       end
-      
+
       it "should consider the documentation to live at https://www.linode.com/api/account/account.#{action}" do
         @linode.documentation_path(Linode.action_path(@linode.class.name, action)).should == "https://www.linode.com/api/account/account.#{action}"
       end
--- spec/linode/image_spec.rb
+++ spec/linode/image_spec.rb
@@ -6,40 +6,40 @@ describe Linode::Image do
     @api_key = 'foo'
     @linode = Linode::Image.new(:api_key => @api_key)
   end
-  
+
   it 'should be a Linode instance' do
     @linode.class.should < Linode
   end
-  
+
   %w(list delete).each do |action|
     it "should allow accessing the #{action} API" do
       @linode.should respond_to(action.to_sym)
     end
-  
+
     describe "when accessing the #{action} API" do
       it 'should allow a data hash' do
         lambda { @linode.send(action.to_sym, {}) }.should_not raise_error(ArgumentError)
       end
-    
+
       it 'should not require arguments' do
         lambda { @linode.send(action.to_sym) }.should_not raise_error(ArgumentError)      
       end
-    
+
       it "should request the image.#{action} action" do
         @linode.expects(:send_request).with {|api_action, data| api_action == "image.#{action}" }
         @linode.send(action.to_sym)
       end
-    
+
       it 'should provide the data hash when making its request' do
         @linode.expects(:send_request).with {|api_action, data| data = { :foo => :bar } }
         @linode.send(action.to_sym, {:foo => :bar})
       end
-    
+
       it 'should return the result of the request' do
-        @linode.expects(:send_request).returns(:bar => :baz)      
-        @linode.send(action.to_sym).should == { :bar => :baz }      
+        @linode.expects(:send_request).returns(:bar => :baz)
+        @linode.send(action.to_sym).should == { :bar => :baz }
       end
-      
+
       it "should consider the documentation to live at https://www.linode.com/api/image/image.#{action}" do
         @linode.documentation_path(Linode.action_path(@linode.class.name, action)).should == "https://www.linode.com/api/image/image.#{action}"
       end
